7bff89ff613cc425ce16a85b0d34405f2881c0db
Siimo <siimokangruoja@hotmail.com>
2024-03-14 14:14:01 +0200

    Update navbar design;

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@
 
 <template>
   <header>
-    <nav class="bg-sky-500 h-14 text-center font-semibold text-2xl pt-3">
+    <nav class="bg-sky-500 h-14 text-center font-semibold text-xl pt-3">
       <router-link class="m-4" to="/">Pealeht</router-link>
       <router-link class="m-4" to="/create">Loo uus</router-link>
     </nav>
