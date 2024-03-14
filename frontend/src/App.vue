b5dbef5d49ae1fb1433f2615a40ae21d31932c7c
Siimo <siimokangruoja@hotmail.com>
2024-03-14 13:53:22 +0200

    Add create link to navbar;

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,9 +3,10 @@
 
 <template>
   <header>
-      <nav class="bg-sky-500 h-14">
-        Navbar
-      </nav>
+    <nav class="bg-sky-500 h-14 text-center font-semibold text-2xl pt-3">
+      <router-link class="m-4" to="/">Pealeht</router-link>
+      <router-link class="m-4" to="/create">Loo uus</router-link>
+    </nav>
   </header>
 
   <RouterView />
